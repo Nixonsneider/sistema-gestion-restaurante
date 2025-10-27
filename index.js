015002f5c6fa65bf619b19e114a4c0d37b3dac58
chipitaps <alonsoesteban1@hotmail.com>
2025-10-27 15:45:34 -0500

    first concept

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', function(){
+   let btn = document.getElementById('enter')
+   
+   btn.addEventListener('click', function(){
+    window.location.href = "admin.html"
+   })
+})
